129a350293891836aaf61a4fdf711f61196dfa97
KiwisCodes <138852649+KiwisCodes@users.noreply.github.com>
2024-11-07 04:07:02 +0000

    done stack

--- lab_w6/stack.c
+++ lab_w6/stack.c
@@ -1,5 +1,6 @@
 
 #include <stdlib.h>
+#include <stdio.h>
 
 /// @brief Implement stack using linked list
 
@@ -14,20 +15,34 @@ struct Stack
     struct Node* top; // also be the head of the linked list
 };
 
+struct Node* createNode(int data)
+{
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
 void push(struct Stack* stack, int data)
 {
-
+    struct Node* newNode = createNode(data);
+    newNode->next = stack->top;
+    stack->top = newNode;
 }
 
 int pop(struct Stack* stack)
 {
-
+    int val = stack->top->data;
+    struct Node* temp = stack->top;
+    stack->top = stack->top->next;
+    free(temp);
+    return val;
 }
 
 // return 1 if the stack is empty otherwise 0
 int is_empty(const struct Stack* stack)
 {
-
+    return stack->top == NULL;
 }
 
 int main()
